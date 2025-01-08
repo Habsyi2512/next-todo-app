6c6255895017ba22fc7e1846b296965075f080a9
Habsyi2512 <githabsyi@gmail.com>
2025-01-09 02:50:56 +0700

    feat: add MySQL configuration for SQLTools and initialize Todo table with sample data

---
+++ todo app.session.sql
@@ -0,0 +1,31 @@
+INSERT INTO Todo (
+    id,
+    title,
+    completed,
+    created_at,
+    updated_at,
+    deleted_at
+  )
+VALUES (
+    id:int,
+    'title:varchar',INSERT INTO Todo (
+        id,
+        title,
+        completed,
+        created_at,
+        updated_at,
+        deleted_at
+      )
+    VALUES (
+        id:int,
+        'title:varchar',
+        'completed:tinyint',
+        'created_at:datetime',
+        'updated_at:datetime',
+        'deleted_at:datetime'
+      );
+    'completed:tinyint',
+    'created_at:datetime',
+    'updated_at:datetime',
+    'deleted_at:datetime'
+  );
