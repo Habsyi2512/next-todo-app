32a0dc1898e698913f872527eccac81cc5e435a5
Habsyi2512 <githabsyi@gmail.com>
2025-01-17 04:03:44 +0700

    feat: update API route parameter handling to use Promise for id extraction

--- src/app/api/todo/[id]/completed/route.ts
+++ src/app/api/todo/[id]/completed/route.ts
@@ -3,14 +3,16 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function PUT(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   const { id } = await params;
   const { completed } = await request.json();
+
   const updatedData = await handleCompletedTodo({
     where: { id: Number(id) },
     data: { completed: completed },
   });
+
   return NextResponse.json(
     { message: "Completed", data: updatedData },
     { status: 200 }
--- src/app/api/todo/[id]/edit-todo-title/route.ts
+++ src/app/api/todo/[id]/edit-todo-title/route.ts
@@ -16,7 +16,7 @@ const editTodoSchema = yup
 
 export async function PATCH(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
     const { id } = await params;
