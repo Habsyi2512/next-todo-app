c89253acbf058ad3a249203367aaa1f58da97417
Habsyi2512 <githabsyi@gmail.com>
2025-01-20 23:08:14 +0700

    feat: add player list placeholder to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,7 @@ export default async function Home() {
       <Suspense fallback={<LoadTodoTemplate />}>
         <IncompleteTodoList todos={todos} />
       </Suspense>
+      <div>daftar pemain</div>
     </>
   );
 }
